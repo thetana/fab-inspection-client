129fe0e65f3ccbb6680c6ab56e476e8690a28c72
thetana <zxsa135@naver.com>
2026-08-17 22:57:07 +0900

    feat: display lot list

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace FabInspectionClient
+namespace FabInspectionClient
 {
     partial class Form1
     {
@@ -23,18 +23,111 @@
         #region Windows Form 디자이너에서 생성한 코드
 
         /// <summary>
-        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 디자이너 지원에 필요한 메서드입니다.
         /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
         /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lotDataGridView = new System.Windows.Forms.DataGridView();
+            this.lotIdColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productCodeColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.processStepColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.statusColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.updatedAtColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.refreshButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.lotDataGridView)).BeginInit();
+            this.SuspendLayout();
+            //
+            // lotDataGridView
+            //
+            this.lotDataGridView.AllowUserToAddRows = false;
+            this.lotDataGridView.AllowUserToDeleteRows = false;
+            this.lotDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lotDataGridView.AutoGenerateColumns = false;
+            this.lotDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.lotDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.lotIdColumn,
+            this.productCodeColumn,
+            this.processStepColumn,
+            this.statusColumn,
+            this.updatedAtColumn});
+            this.lotDataGridView.Location = new System.Drawing.Point(12, 12);
+            this.lotDataGridView.MultiSelect = false;
+            this.lotDataGridView.Name = "lotDataGridView";
+            this.lotDataGridView.ReadOnly = true;
+            this.lotDataGridView.RowHeadersVisible = false;
+            this.lotDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.lotDataGridView.Size = new System.Drawing.Size(776, 381);
+            this.lotDataGridView.TabIndex = 0;
+            //
+            // lotIdColumn
+            //
+            this.lotIdColumn.DataPropertyName = "LotId";
+            this.lotIdColumn.HeaderText = "LOT ID";
+            this.lotIdColumn.Name = "lotIdColumn";
+            this.lotIdColumn.ReadOnly = true;
+            //
+            // productCodeColumn
+            //
+            this.productCodeColumn.DataPropertyName = "ProductCode";
+            this.productCodeColumn.HeaderText = "Product Code";
+            this.productCodeColumn.Name = "productCodeColumn";
+            this.productCodeColumn.ReadOnly = true;
+            //
+            // processStepColumn
+            //
+            this.processStepColumn.DataPropertyName = "ProcessStep";
+            this.processStepColumn.HeaderText = "Process Step";
+            this.processStepColumn.Name = "processStepColumn";
+            this.processStepColumn.ReadOnly = true;
+            //
+            // statusColumn
+            //
+            this.statusColumn.DataPropertyName = "Status";
+            this.statusColumn.HeaderText = "Status";
+            this.statusColumn.Name = "statusColumn";
+            this.statusColumn.ReadOnly = true;
+            //
+            // updatedAtColumn
+            //
+            this.updatedAtColumn.DataPropertyName = "UpdatedAt";
+            this.updatedAtColumn.HeaderText = "Updated At";
+            this.updatedAtColumn.Name = "updatedAtColumn";
+            this.updatedAtColumn.ReadOnly = true;
+            //
+            // refreshButton
+            //
+            this.refreshButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.refreshButton.Location = new System.Drawing.Point(688, 405);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(100, 33);
+            this.refreshButton.TabIndex = 1;
+            this.refreshButton.Text = "새로 고침";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            //
+            // Form1
+            //
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.lotDataGridView);
+            this.MinimumSize = new System.Drawing.Size(600, 300);
+            this.Text = "LOT 목록";
+            ((System.ComponentModel.ISupportInitialize)(this.lotDataGridView)).EndInit();
+            this.ResumeLayout(false);
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView lotDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lotIdColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productCodeColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn processStepColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn statusColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn updatedAtColumn;
+        private System.Windows.Forms.Button refreshButton;
     }
 }
-
--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +16,78 @@ namespace FabInspectionClient
 {
     public partial class Form1 : Form
     {
+        private const string ApiBaseUrl = "http://localhost:8080";
+        private static readonly HttpClient HttpClient = new HttpClient
+        {
+            BaseAddress = new Uri(ApiBaseUrl)
+        };
+
         public Form1()
         {
             InitializeComponent();
+            Load += Form1_Load;
+            refreshButton.Click += RefreshButton_Click;
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await LoadLotsAsync();
+        }
+
+        private async void RefreshButton_Click(object sender, EventArgs e)
+        {
+            await LoadLotsAsync();
+        }
+
+        private async Task LoadLotsAsync()
+        {
+            refreshButton.Enabled = false;
+
+            try
+            {
+                using (HttpResponseMessage response = await HttpClient.GetAsync("/api/lots"))
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    using (Stream responseStream = await response.Content.ReadAsStreamAsync())
+                    {
+                        var serializer = new DataContractJsonSerializer(typeof(List<LotDto>));
+                        var lots = serializer.ReadObject(responseStream) as List<LotDto>;
+                        lotDataGridView.DataSource = lots ?? new List<LotDto>();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "LOT 목록을 조회하지 못했습니다.\n" + ex.Message,
+                    "조회 오류",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            finally
+            {
+                refreshButton.Enabled = true;
+            }
+        }
+
+        [DataContract]
+        private sealed class LotDto
+        {
+            [DataMember(Name = "lotId")]
+            public string LotId { get; set; }
+
+            [DataMember(Name = "productCode")]
+            public string ProductCode { get; set; }
+
+            [DataMember(Name = "processStep")]
+            public string ProcessStep { get; set; }
+
+            [DataMember(Name = "status")]
+            public string Status { get; set; }
+
+            [DataMember(Name = "updatedAt")]
+            public string UpdatedAt { get; set; }
         }
     }
 }
